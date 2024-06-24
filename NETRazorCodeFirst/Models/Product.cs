1f8f59413417244394e53d4b02261b669ef94508
EngelReyes23 <EngelReyes23@Gmail.com>
2024-06-24 17:53:34 -0600

    Add Product model

---
+++ NETRazorCodeFirst/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NETRazorCodeFirst.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Display(Name = "Product Name")]
+        public required string ProductName { get; set; }
+
+        [Display(Name = "Product Description")]
+        public required string ProductDescription { get; set; }
+
+        [Display(Name = "Product Price")]
+        public required decimal ProductPrice { get; set; }
+
+        [Display(Name = "Product Quantity")]
+        public required decimal ProductQuantity { get; set; }
+
+        [Display(Name = "Date Created")]
+        public DateTime DateCreated { get; set; }
+    }
+}
