3018bc9ff060a98ec387e962e33d777973b1a86c
EngelReyes23 <EngelReyes23@Gmail.com>
2024-06-24 21:35:03 -0600

    Add Create Product Page

---
+++ NETRazorCodeFirst/Pages/Products/Create.cshtml.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using NETRazorCodeFirst.Data;
+using NETRazorCodeFirst.Models;
+
+namespace NETRazorCodeFirst.Pages.Products
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CreateModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Product Product { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<ActionResult> OnPost()
+        {
+            if (ModelState.IsValid) {
+                Product.DateCreated = DateTime.Now;
+
+                _context.Product.Add(Product);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
+            } else {
+                return Page();
+            }
+        }
+    }
+}
